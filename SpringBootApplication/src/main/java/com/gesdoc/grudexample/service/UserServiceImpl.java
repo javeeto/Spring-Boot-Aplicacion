80161c7306c70f6f5e9305b0605df26d3637b451
javeeto <javeeto@gmail.com>
2021-11-23 21:27:04 -0500

    Actualizar usuario, modo edición en formulario

--- SpringBootApplication/src/main/java/com/gesdoc/grudexample/service/UserServiceImpl.java
+++ SpringBootApplication/src/main/java/com/gesdoc/grudexample/service/UserServiceImpl.java
@@ -28,8 +28,21 @@ public class UserServiceImpl implements UserService {
 		return true;
 	}
 	
+	private boolean checkEmailAvailable(User user) throws Exception {
+		Optional<User> userFound = usersRepo.findByEmail(user.getEmail());
+		if(userFound.isPresent()) {
+			throw new Exception("Email ya registrado");
+		}
+		return true;
+	}
+	
 	
 	private boolean checkPasswordValid(User user) throws Exception {
+		if (user.getConfirmPassword() == null || user.getConfirmPassword().isEmpty()) {
+			throw new Exception("Confirm Password es obligatorio");
+		}
+		
+		
 		if(!user.getPassword().equals(user.getConfirmPassword())) {
 			throw new Exception("Clave y confirmación no son iguales");
 		}
@@ -40,14 +53,41 @@ public class UserServiceImpl implements UserService {
 	public User createUser(User user) throws Exception {
 		// TODO Auto-generated method stub
 		if(this.checkPasswordValid(user)&&
-				this.checkUsernameAvailable(user)) {
+				this.checkUsernameAvailable(user)&&
+				this.checkEmailAvailable(user)) {
 			user = usersRepo.save(user);
 			
 		}
 		
 		return user;
 	}
+
+	@Override
+	public User getUserById(Long id) throws Exception {
+		// TODO Auto-generated method stub
+		User user =  usersRepo.findById(id).orElseThrow(() -> new Exception("Usuario no existe"));
+		return user;
+	}
+
+	@Override
+	public User updateUser(User fromUser) throws Exception {
+		User toUser = this.getUserById(fromUser.getId());
+		this.mapUser(fromUser, toUser);
+		return usersRepo.save(toUser);
+	}
 	
+	/**
+	 * Map everything but the password.
+	 * @param from
+	 * @param to
+	 */
+	protected void mapUser(User from,User to) {
+		to.setUsername(from.getUsername());
+		to.setFirstName(from.getFirstName());
+		to.setLastName(from.getLastName());
+		to.setEmail(from.getEmail());
+		to.setRoles(from.getRoles());
+	}
 	
 
 }
